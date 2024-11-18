02627d0ce972c09546654a5e80fdb20df8dde43e
TwistedUmbrellaX <1173913+AbandonedCart@users.noreply.github.com>
2024-11-17 19:43:45 -0500

    Remove window state plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,6 @@ pub fn run() {
     let builder = tauri::Builder::default();
 
     builder
-        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![commands::close_splashscreen])
         .run(context)
         .expect("error while running tauri application");
